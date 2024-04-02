7532cb4518ee02b000241b98bd3f364e72895d2e
CarlosEduardoCabreraMiranda <cecabrera-2022a@corhuila.edu.co>
2024-04-02 08:40:09 -0500

    Subida de codigo base login

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,32 @@
 import './ExploreContainer.css';
+import { IonInput, IonItem, IonList, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/react';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Logueate</IonCardTitle>
+        </IonCardHeader>
+
+        <IonCardContent><IonItem>
+          <IonInput label="Username" placeholder="Username" maxlength={20}></IonInput>
+        </IonItem>
+
+          <IonItem>
+            <IonInput label="Password" placeholder="Password" maxlength={20}></IonInput>
+          </IonItem>
+
+        </IonCardContent>
+
+        <IonButton>Submit</IonButton>
+      </IonCard>
+      <IonList>
+      
+      </IonList>
+
     </div>
   );
 };
